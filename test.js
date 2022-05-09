b2e3776c8247d031aaa15e4dc3fd07ac8eb9dfb0
uzrnem <bhagyeshsunilpatel@gmail.com>
2022-05-09 08:21:21 +0530

    updated version

--- test.js
+++ test.js
@@ -1,35 +1,32 @@
-var {verify} = require('smooth-validator');
-validators = {
-  'id' : 'numeric',
-  'name' : 'alpha|alphanumeric|max:20',
-  'nice_name' : 'nullable|uppercase',
-  'home_name' : 'nullable|lowercase',
-  'email' : 'min:5|email',
-  'estado' : 'in:show,hide',
-  'address' : 'required',
-  'created_at' : 'required|date'
-}
-data = {
-  id : '23l',
-  name : 'Er. Bhagyesh Sunil Patel',
-  home_name : '',
-  nice_name : 'Yash',
-  email : '',
-  estado : 'show',
-  created_at : '2018-09-10'
+const parser = require('./smooth');
+
+completed = {
+  id: 'numeric|between:-24,30',
+  name: 'uppercase',
+  home_name: 'required|lowercase',
+  nice_name: 'uppercase',
+  created_at: 'date',
+  isEmployee: 'boolean',
+  email: 'email',
+  regex: 'alpha|alphanumeric|alphaunderscope|alphadash',
+  compare: 'max:20|min:5|size:10',
+  estado: 'same:status|not_in:hide,show1|in:hide,show',
 }
 
+var validate = parser(completed)
 data1 = {
-  id : 23,
-  name : 'Bhagyesh Sunil Patel',
+  id : -23.233,
+  name : 'BHAGYESH PATEL',
   home_name : 'yash',
   nice_name : 'YASH',
   email : 'uzrnem@gmail.com',
+  compare: 'bhagyeshsp',
   estado : 'show',
   status : 'show',
   address: 'pune',
+  isEmployee: 'true',
   created_at : new Date()
 }
-
-console.log(verify(data, validators))
-console.log(verify(data1, validators))
+var res = validate(data1);
+console.log(res)
+  
